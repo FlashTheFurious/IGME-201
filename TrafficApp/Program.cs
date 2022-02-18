cd2aece15445aed6f6f6d19b47d522a30aa1ea7d
Tarnav Bhasin <tarnavbhasin@gmaill.com>
2022-02-18 17:24:48 -0500

    Added VehicleLibrary and TrafficApp

---
+++ TrafficApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleLibrary;
+
+namespace TrafficApp
+{
+
+    // Class: Program
+    // Author: Tarnav Bhasin
+    // Purpose: To construct a new method that accepts a new object that inherits from
+    //          the IPassengerCarrier interface
+    // Restrictions: None
+    class Program
+    {
+
+        // Method: Main
+        // Purpose: Declare a new instances of Compact() and _424DoubleBogey()
+        //          Call AddPassenger while passing in the Compact() object
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+            Compact newCar = new Compact();
+            _424DoubleBogey newBogey = new _424DoubleBogey();
+
+            AddPassenger(newCar);
+            //AddPassenger(newBogey);
+            //The code shoots an error because it recognizes that newBogey does not
+            //inherit from the IPassenger interface
+        }
+
+        // Method: AddPassenger
+        // Parameters: Accepts any object that inherits from the IPassengerCarrier interface
+        // Purpose: Calls the LoadPassenger method on the Object.
+        //          Prints the Object name to the console
+        // Restrictions: None
+        public static void AddPassenger(IPassengerCarrier objectName)
+        {
+            objectName.LoadPassenger();
+            Console.WriteLine(objectName.ToString());
+        }
+    }
+}
---
+++ VehicleLibrary/Class1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// NameSpace: VehicleLibrary
+// Author: Tarnav Bhasin
+// Purpose: To construct empty classes as described in the UML diagram
+//          This demonstrates inheritance and how to inherit from interfaces
+// Restrictions: None
+namespace VehicleLibrary
+{
+
+    abstract public class Vehicle
+    {
+        public virtual void LoadPassenger()
+        {
+
+        }
+    }
+
+    abstract public class Train : Vehicle
+    {
+
+    }
+    abstract public class Car : Vehicle
+    {
+
+    }
+    public class SUV : Car, IPassengerCarrier
+    {
+
+    }
+    public class Pickup : Car, IPassengerCarrier, IHeavyLoadCarrier
+    {
+
+    }
+    public class Compact : Car, IPassengerCarrier
+    {
+
+    }
+    public class FreightTrain : Train, IHeavyLoadCarrier
+    {
+
+    }
+    public class _424DoubleBogey : Train, IHeavyLoadCarrier
+    {
+
+    }
+    public class PassengerTrain : Train, IPassengerCarrier
+    {
+
+    }
+    public interface IPassengerCarrier
+    {
+        void LoadPassenger();
+
+    }
+    public interface IHeavyLoadCarrier
+    {
+
+
+    }
+
+}
