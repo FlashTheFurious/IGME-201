a77b21b293beaaa3b5b4067c5555de17607aa9db
Tarnav Bhasin <tarnavbhasin@gmaill.com>
2022-02-22 17:48:27 -0500

    renamed a folder

---
+++ HW12_Q3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW12_Q3
+{
+    public class MyClass
+    {
+        private string myString = "ExampleString";
+        public string MyString
+        {
+            set
+            {
+                myString = value;
+            }
+        }
+        public virtual string GetString()
+        {
+            return myString;
+        }
+    }
+
+    class MyDerivedClass : MyClass
+    {
+        public override string GetString()
+        {
+            string resultString = base.GetString();
+            resultString += " (output from the derived class)";
+
+            return resultString;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDerivedClass newDerivedObject = new MyDerivedClass();
+            Console.WriteLine(newDerivedObject.GetString());
+
+        }
+    }
+}
