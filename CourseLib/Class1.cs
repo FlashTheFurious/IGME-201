24d77fa54544fd56b40c06fb93b6a887344c8d04
Tarnav Bhasin <tarnavbhasin@gmaill.com>
2022-02-23 19:47:26 -0500

    Added People Library and PeopleApp

--- CourseLib/Class1.cs
+++ CourseLib/Class1.cs
@@ -18,7 +18,47 @@ namespace CourseLib
     {
         public SortedList<string, Course> sortedList = new SortedList<string, Course>();
         public string courseCode;
+        // indexer property allows array access to sortedList via the class object
+        // and catching missing keys and duplicate key exceptions 
+        // notice the indexer property definition shows how it will be used in the calling code:
+        // if we have:
+        //     People people;
+        // then we can call:
+        //     people[email] to access the Person object with that email address
+        // and value will be the Person object (person) being added to the list in the case of:
+        //     people[email] = person;
+        public Course this[string courseCode]
+        {
+            get
+            {
+                Course returnVal;
+                try
+                {
+                    returnVal = (Course)sortedList[courseCode];
+                }
+                catch
+                {
+                    returnVal = null;
+                }
 
+                return (returnVal);
+            }
+
+            set
+            {
+                try
+                {
+                    // we can add to the list using these 2 methods
+                    //      sortedList.Add(email, value);
+                    sortedList[courseCode] = value;
+                }
+                catch
+                {
+                    // an exception will be raised if an entry with a duplicate key is added
+                    // duplicate key handling (currently ignoring any exceptions)
+                }
+            }
+        }
         public Courses()
         {
             Course thisCourse;
@@ -76,6 +116,11 @@ namespace CourseLib
         public string description;
         public string teacherEmail;
         public Schedule schedule;
+
+        public Course()
+        {
+
+        }
 
         public Course(string courseCode, string description)
         {
