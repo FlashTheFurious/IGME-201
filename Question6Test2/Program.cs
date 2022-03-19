b1ba302c2b90a7a5991b2f634fdb47581fb8a6ce
Tarnav Bhasin <tarnavbhasin@gmaill.com>
2022-03-18 20:28:08 -0400

    Added question 7, for 4,5,6,7

---
+++ Question6Test2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using question4Test2;
+
+namespace Question6Test2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Make an object of Tardis and PhoneBooth
+            Tardis timeMachine = new Tardis();
+
+            PhoneBooth normalBooth = new PhoneBooth();
+
+            //Pass the objects to UsePhone
+            UsePhone(timeMachine);
+            UsePhone(normalBooth);
+
+        }
+        static void UsePhone(object obj)
+        {
+            //Casting the object as an interface to access interface methods
+            IPhoneInterface iReference = (IPhoneInterface)obj;
+
+            iReference.MakeCall();
+            iReference.HangUp();
+
+            //Check what an object is related to, then call correct method
+
+            if (obj is Tardis)
+            {
+                Tardis tardisR = (Tardis)obj;
+                tardisR.TimeTravel();
+            }
+            else if (obj is PhoneBooth)
+            {
+                PhoneBooth phoneBoothR = (PhoneBooth)obj;
+                phoneBoothR.OpenDoor();
+            }
+        }
+
+    }
+}
