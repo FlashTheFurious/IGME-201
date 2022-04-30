e3b18f6fa9e88c1094be5de89e82891d3c4b8dbc
Tarnav Bhasin <tarnavbhasin@gmaill.com>
2022-04-29 21:34:12 -0400

    updates

--- PetApp/Program.cs
+++ PetApp/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Timers;
 using System.Threading.Tasks;
 
 namespace PetApp
@@ -156,7 +157,14 @@ namespace PetApp
 
     public class Dog : Pet, IDog
     {
+
         public string license;
+
+        public static void EvictCat(object sender, ElapsedEventArgs e)
+        {
+
+        }
+
         public override void Eat()
         {
             Console.WriteLine(this.Name + " is hungry. Needs juicy bone!");
@@ -203,6 +211,10 @@ namespace PetApp
     {
         static void Main(string[] args)
         {
+            Timer myTimer = new Timer(20000);
+            myTimer.Elapsed += new ElapsedEventHandler(Dog.EvictCat);
+            myTimer.Start();
+
             //1
             Pet thisPet = null;
             Dog dog = null;
@@ -219,6 +231,7 @@ namespace PetApp
             //4
             for (int i = 0; i < 50; i++)
             {
+                ReturnHereIfNull:
                 int iNewRandom = rand.Next(1, 11);
 
                 // 1 in 10 chance of adding an animal
@@ -226,12 +239,36 @@ namespace PetApp
                 {
                     if (rand.Next(0, 2) == 0)
                     {
+                        string dogName;
+                        int dogAge;
+                        string dogLz;
+                        Console.WriteLine("You bought a Dog!");
+                        Console.Write("Name the Dog-> ");
+                        dogName = Console.ReadLine();
+                        Console.Write("What's the Dog's age? -> ");
+                        dogAge = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("What's the Dog's license ID? -> ");
+                        dogLz = Console.ReadLine();
+
+                        dog = new Dog(dogLz, dogName, dogAge);
                         // add a dog
                         pets.Add(dog);
 
                     }
                     else
                     {
+                        string catName;
+                        int catAge;
+
+                        Console.WriteLine("You bought a Cat!");
+                        Console.Write("Name the Cat-> ");
+                        catName = Console.ReadLine();
+                        Console.Write("What's the Cat's age? -> ");
+                        catAge = Convert.ToInt32(Console.ReadLine());
+
+                        cat = new Cat();
+                        cat.Name = catName;
+                        cat.age = catAge;
                         // else add a cat
                         pets.Add(cat);
                     }
@@ -239,8 +276,69 @@ namespace PetApp
                 else
                 {
                     // choose a random pet from pets and choose a random activity for the pet to do
+                    thisPet = pets[rand.Next(0, pets.Count)];
 
+                    if (thisPet == null)
+                    {
+
+                        goto ReturnHereIfNull;
+                    }
+                    else
+                    {
+                        if (thisPet.GetType() == typeof(Dog))
+                        {
+                            iDog = (IDog)thisPet;
+                            int randomActivity = rand.Next(1, 6);
+
+                            if (randomActivity == 1)
+                            {
+                                iDog.Bark();
+                            }
+                            else if (randomActivity == 2)
+                            {
+                                iDog.NeedWalk();
+                            }
+                            else if (randomActivity == 3)
+                            {
+                                iDog.GotoVet();
+                            }
+                            else if (randomActivity == 4)
+                            {
+                                iDog.Play();
+                            }
+                            else if (randomActivity == 5)
+                            {
+                                iDog.Eat();
+                            }
+                        }
+                        else if (thisPet.GetType() == typeof(Cat))
+                        {
+                            iCat = (ICat)thisPet;
+                            int randomActivity = rand.Next(1, 5);
+
+                            if (randomActivity == 1)
+                            {
+                                iCat.Purr();
+                            }
+                            else if (randomActivity == 2)
+                            {
+                                iCat.Scratch();
+                            }
+                            else if (randomActivity == 3)
+                            {
+                                iCat.Eat();
+                            }
+                            else if (randomActivity == 4)
+                            {
+                                iCat.Play();
+                            }
+
+                        }
+
+                    }
                 }
+
+
 
             }
 
